dd017a57d16dc2eb05906b954c7a5ddd50e5de77
Sahil Singla <f2016103@pilani.bits-pilani.ac.in>
2018-08-10 12:46:00 +0530

    Added access.php

--- block_test_creator.php
+++ block_test_creator.php
@@ -5,6 +5,7 @@ class block_test_creator extends block_base {
         $this->title = get_string('pluginname', 'block_test_creator');
     }
     public function get_content() {
+    	$this->content = new stdClass();
         $output = $this->page->get_renderer('block_test_creator');
         $add_new_test = new \block_test_creator\output\add_new_test();
         $this->content->text = $output->render($add_new_test);
---
+++ view.php
@@ -0,0 +1,5 @@
+<?php 
+
+class question_bank_view extends \core_question\bank\view {
+    
+}
