100525f2e600d7d67db70574af3b72a911c6e7df
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2018-08-11 10:33:24 +0530

    view: Require quiz name.

--- view.php
+++ view.php
@@ -8,7 +8,7 @@ global $DB;
 // Check for all required variables.
 // $courseid = required_param('courseid', PARAM_INT);
 $courseid = 1; 
- 
+$quiz_name = required_param('name', PARAM_RAW);
 if (!$course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('invalidcourse', 'block_test_creator', $courseid);
 }
