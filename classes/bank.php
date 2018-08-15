c4ea21726586119da777366d2bc19298dc14cb2e
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2018-08-15 15:58:20 +0530

    Remove add_to_quiz column;

--- classes/bank.php
+++ classes/bank.php
@@ -6,6 +6,40 @@ require_once(__DIR__.'/search/topic_condition.php');
 require_once(__DIR__.'/search/subtopic_condition.php');
 class test_creator_bank_view extends \mod_quiz\question\bank\custom_view{
 
+    protected function wanted_columns() {
+        global $CFG;
+
+        if (empty($CFG->quizquestionbankcolumns)) {
+            $quizquestionbankcolumns = array(
+                'checkbox_column',
+                'question_type_column',
+                'question_name_text_column',
+                'preview_action_column',
+            );
+        } else {
+            $quizquestionbankcolumns = explode(',', $CFG->quizquestionbankcolumns);
+        }
+
+        foreach ($quizquestionbankcolumns as $fullname) {
+            if (!class_exists($fullname)) {
+                if (class_exists('mod_quiz\\question\\bank\\' . $fullname)) {
+                    $fullname = 'mod_quiz\\question\\bank\\' . $fullname;
+                } else if (class_exists('core_question\\bank\\' . $fullname)) {
+                    $fullname = 'core_question\\bank\\' . $fullname;
+                } else if (class_exists('question_bank_' . $fullname)) {
+                    debugging('Legacy question bank column class question_bank_' .
+                            $fullname . ' should be renamed to mod_quiz\\question\\bank\\' .
+                            $fullname, DEBUG_DEVELOPER);
+                    $fullname = 'question_bank_' . $fullname;
+                } else {
+                    throw new coding_exception("No such class exists: $fullname");
+                }
+            }
+            $this->requiredcolumns[$fullname] = new $fullname($this);
+        }
+        return $this->requiredcolumns;
+    }
+
     public function display($tabname, $page, $perpage, $cat,
             $recurse, $showhidden, $showquestiontext, $tagids = [], $subjectids = [], $topicids = [],
             $subtopicids = []) {
